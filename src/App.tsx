e5d3823fa782bbae1a266a171bc898d47897086d
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-08-02 20:43:21 +0300

    code separation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-// import logo from './logo.svg';
+
+import Parcel from './Parcel';
+import Offices from './Offices';
 import './App.css';
 
 function App() {
@@ -7,23 +9,10 @@ function App() {
     <div className="App">
       <div className='operation'>
       <button className='check'>Перевірити ТТН</button>
-      <button className='offices'>Список відділень</button>
+        <button className='offices'>Список відділень</button>
       </div>
-      <div className='get'>
-        <input type="text" className='input' />
-      <button className='get-btn'>Get status TTN</button>
-      </div>
-      <div className='info'>
-      <div className='details'>
-        <p className='details-text'>Дані посилки</p>
-      </div>
-      <div className='history'>
-          <strong className='history-text'>Історія</strong>
-          <ul className='history-list'>
-            <li className='history-item'>20400048799002</li>
-          </ul>
-      </div>
-      </div>
+      {/* <Parcel /> */}
+      <Offices/>
     </div>
   );
 }
---
+++ src/Offices.tsx
@@ -0,0 +1,9 @@
+const Offices = () => {
+    return (
+        <div className='details'>
+            <strong className='details-text'>Відділення Нова пошта</strong>
+        </div>
+    )
+}
+
+export default Offices
