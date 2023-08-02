84ddd3b3c73fb641dd7736c151665da8c09e4470
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-08-02 18:42:55 +0300

    Add html

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Перевірити ТТН</button>
+      <button>Список відділень</button>
+      <input type="text" />
+      <button>Get status TTN</button>
+      <div>
+        <p>Дані посилки</p>
+      </div>
+      <div>
+        <p>Історія</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
